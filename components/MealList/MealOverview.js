672068d0aafcbd9ce647e88f0a0460d1ced3afee
alegarn <alexgarnier78310@protonmail.com>
2023-07-06 06:24:00 +0200

    list favorite meals

--- components/MealList/MealOverview.js
+++ components/MealList/MealOverview.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, StyleSheet, Pressable, Platform } from 'react-native';
-import MealDetails from './MealDetails';
+import MealDetails from '../MealDetails';
 
 /* import { useNavigation } from '@react-navigation/native';
 import useRoute from '@react-navigation/native'; */
