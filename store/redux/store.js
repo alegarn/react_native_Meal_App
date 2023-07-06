f9090e00a826e3c68399b56bb1c6feaf44a3947c
alegarn <alexgarnier78310@protonmail.com>
2023-07-06 06:54:41 +0200

    Working with Redux Slices

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import favoritesReducers from './favorites'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoritesReducers
+  },
 })
